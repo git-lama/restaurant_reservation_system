37a40993c31dfec2543399f6d9fcfad61e24f475
Amal Subhash <69242143+git-lama@users.noreply.github.com>
2024-09-18 15:03:39 -0300

    Improve Table Reservation logic

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,16 +1,10 @@
 class ReservationsController < ApplicationController
   def create
     restaurant = Restaurant.find(params[:restaurant_id])
-    reservation_start_time = reservation_params[:start_time].to_time
-    reservation_end_time = (reservation_start_time + reservation_params[:duration].to_i.minutes).to_time
 
-    table = restaurant.tables
-              .where("size >= ?", reservation_params[:party_size])
-              .left_joins(:reservations)
-              .where("reservations.start_time IS NULL OR NOT (reservations.start_time < ? AND reservations.start_time + reservations.duration * interval '1 minute' > ?)",
-                      reservation_end_time, reservation_start_time)
-              .first
-    # check both start time and end time of existing reservations to ensure the new reservation doesn't conflict
+    tables = restaurant.tables.where("size >= ?", reservation_params[:party_size]).to_a
+
+    table = table_allocation(tables, reservation_params[:party_size], reservation_params[:start_time])
 
     if table
       reservation = table.reservations.create!(reservation_params)
@@ -21,7 +15,23 @@ class ReservationsController < ApplicationController
   end
 
   private
+
   def reservation_params
     params.require(:reservation).permit(:party_size, :start_time, :duration)
+  end
+
+  def table_allocation(tables, party_size, start_time)
+    # Sort tables by size and availability, then apply logic
+    tables.sort_by(&:size).detect do |table|
+      # Check if the table is available for the requested time
+      table.reservations.none? { |r| time_overlap?(r.start_time, r.duration, start_time) }
+    end
+  end
+
+  def time_overlap?(reservation_start, reservation_duration, requested_start)
+    reservation_end = reservation_start + reservation_duration.minutes
+    requested_end = requested_start + reservation_duration.minutes
+    # check both start time and end time of existing reservations to ensure the new reservation doesn't conflict
+    reservation_start <= requested_end && requested_start <= reservation_end
   end
 end
